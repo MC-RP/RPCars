57f628cd6316a5a27b609f534a31c3eeacd28e7c
Fozie <foziegameing@gmail.com>
2014-09-08 13:05:56 +0200

    Update: Working CarEvent Packet interseceptor for testing

--- src/com/mcrp/rpcars/CarEvents.java
+++ src/com/mcrp/rpcars/CarEvents.java
@@ -1,10 +1,13 @@
 package com.mcrp.rpcars;
 
 import org.bukkit.event.Listener;
+
 import org.bukkit.plugin.Plugin;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.Packets.Client;
 
 public class CarEvents extends PacketAdapter implements Listener{
 	
@@ -14,6 +17,15 @@ public class CarEvents extends PacketAdapter implements Listener{
 	public CarEvents(Plugin plugin, Cars carManager) {
 		super(plugin, PacketType.Play.Client.STEER_VEHICLE);
 		this.carManager = carManager;
+	}
+	
+	@Override
+	public void onPacketReceiving(PacketEvent e) {
+		if(e.getPacketType() != PacketType.Play.Client.STEER_VEHICLE)
+			return;
+		float forwardModifier = e.getPacket().getFloat().read(1);
+		float sidewayModifier = e.getPacket().getFloat().read(0);
+		e.getPlayer().sendMessage("STEERING: " + forwardModifier + ", " + sidewayModifier);
 	}
 
 }
